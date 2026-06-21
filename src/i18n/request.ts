addcd54d27cb829dbdf5a6ea4377efc3a9dec6bd
Moonteiro <owagnerlucas0@gmail.com>
2026-06-21 10:32:56 -0300

    feat: adiciona configuração das rotas e requisições pro portifólio bilingue

---
+++ src/i18n/request.ts
@@ -0,0 +1,13 @@
+import { getRequestConfig } from "next-intl/server";
+import { routing } from "./routing";
+
+export default getRequestConfig(async ({ requestLocate }) => {
+  let locale = await requestLocate;
+  if (!locale || !routing.locales.includes(locale as "pt" | "en")) {
+    locale = routing.defaultLocale;
+  }
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default,
+  };
+});
